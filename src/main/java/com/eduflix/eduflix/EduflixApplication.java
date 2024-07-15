5a4a47b43bb8f25911d87c293e01e690055a37a5
The Darik <thedarik2021@gmail.com>
2024-07-15 12:24:56 +0500

    Samandar first commit

---
+++ src/main/java/com/eduflix/eduflix/EduflixApplication.java
@@ -0,0 +1,13 @@
+package com.eduflix.eduflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduflix/eduflix/EduflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduflix.eduflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
