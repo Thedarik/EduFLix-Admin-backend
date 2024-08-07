690b9fed574f40ab72e13ec968ecd3ddc40b93c4
islomTurgunpolatov <turgunpolatovislom5@gmail.com>
2024-08-07 16:34:40 +0500

    updated properties file

--- src/main/java/com/eduflix/eduflix/EduflixApplication.java
+++ src/main/java/com/eduflix/eduflix/EduflixApplication.java
@@ -1,22 +1,15 @@
 package com.eduflix.eduflix;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.messaging.FirebaseMessaging;
-import io.jsonwebtoken.JwtParser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
-
-import java.io.IOException;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.eduflix.eduflix")
 public class EduflixApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EduflixApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EduflixApplication.class, args);
+    }
 
 }
